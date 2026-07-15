32c37945801261bbff7990f4e66a5b05c6abc337
rathod-gaurav <rathod.gauravvinod@gmail.com>
2026-07-15 00:23:54 -0500

    corrected the latent heat term bug in the explicit solver

--- PhaseFieldModeling/Solidification1D-Stefan/explicit/main.cpp
+++ PhaseFieldModeling/Solidification1D-Stefan/explicit/main.cpp
@@ -17,7 +17,7 @@ int main(){
     constexpr unsigned int Nne = 2;
 
     //Number of timesteps to solve for
-    unsigned int NT = 1000;
+    unsigned int NT = 10000;
     unsigned int incrSteps = 1;
 
     //Quadrature order
@@ -36,12 +36,12 @@ int main(){
     double Tcold = 263.15; //K
 
     double sigma = 0.033; //J/m2
-    double mu = 1e-4; //m/s-K
+    double mu = 1e-8; //m/s-K
 
     //Assumptions
     double epsilon = 5*1e-4;
     //Derived quantities
-    double W = 1.0;
+    double W = 0.5;
     double delta = epsilon*sqrt(2.0/W);
     double lambda = (5.0/8.0)*(epsilon*sqrt(2*W)*rho*((Cs+Cl)/2)*Tm)/LatentHeat;
     double tau = (15*rho*((Cs+Cl)/2)*Tm)/(4*mu*LatentHeat);
@@ -120,7 +120,7 @@ int main(){
     //Initialize the phi and T global vectors
     Eigen::VectorXd phi = Eigen::VectorXd::Zero(mesh.Nnodes());
     Eigen::VectorXd T = Eigen::VectorXd::Zero(mesh.Nnodes());
-    double X0 = 0.01;
+    double X0 = 0.007;
     for(unsigned int i = 0 ; i < mesh.Nnodes() ; i++){
         phi(i) = 0.5*(1 - std::tanh((mesh.nodes[i].x1 - X0)/delta));
         T(i) = Tm;
